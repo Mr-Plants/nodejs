6aad79cac92ece99037a9b5a7cb7e91674c0c8c7
weiyb <weiyb@jiedaibao.com>
2019-05-17 10:33:19 +0800

    learn puppeteer

--- puppeteer/exportPdf.js
+++ puppeteer/exportPdf.js
@@ -1,10 +1,10 @@
-const screenshot = require('puppeteer');
+const puppeteer = require('puppeteer');
 
 async function init() {
-    const pupperteerConfig = {
+    const puppeteerConfig = {
         executablePath: './node_modules/Chromium.app/Contents/MacOS/Chromium'
     };
-    const browser = await screenshot.launch(pupperteerConfig);
+    const browser = await puppeteer.launch(puppeteerConfig);
     const page = await browser.newPage();
     await page.goto('https://www.jianshu.com/u/40909ea33e50');
     await page.pdf({
