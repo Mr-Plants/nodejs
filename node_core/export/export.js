abf0be894d89ce781e51255d11bead7c2f3470a8
weiyb <weiyb@jiedaibao.com>
2019-06-25 14:45:07 +0800

    add module export compare

--- node_core/export/export.js
+++ node_core/export/export.js
@@ -17,3 +17,9 @@ console.log(exports)
 
 // 当两者同时出现时，只取module.exports，无论两者是否有同名属性或方法
 // 当只出现exports时，exports会向module.exports进行单向合并
+
+/**
+ * module.exports才是真正的接口，exports只不过是它的一个辅助工具。　最终返回给调用的是module.exports而不是exports。
+ * 所有的exports收集到的属性和方法，都赋值给了Module.exports。当然，这有个前提，就是module.exports本身不具备任何属性和方法。
+ * 如果，module.exports已经具备一些属性和方法，那么exports收集来的信息将被忽略。
+ */
