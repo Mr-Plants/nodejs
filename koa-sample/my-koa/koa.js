dd9d20747d5d3efa66a71ecc60974d4eb12c55d2
weiyb <weiyb@jiedaibao.com>
2020-06-05 21:20:48 +0800

    koa cook

--- koa-sample/my-koa/koa.js
+++ koa-sample/my-koa/koa.js
@@ -15,32 +15,58 @@ class Koa {
    */
   use(middleware) {
     this.middlewares.push(middleware)
+    return this
   }
 
   listen(...args) {
-    const server = http.createServer((req, res) => {
+    const server = http.createServer(async (req, res) => {
       const ctx = this.createContext(req, res)
       const fn = this.compose(this.middlewares)
+      await fn(ctx)
       res.end(ctx.body)
     })
     server.listen(...args)
   }
 
+  /**
+   * todo 构建上下文
+   * @param req
+   * @param res
+   * @returns {{readonly method: *, body, readonly url: *}}
+   */
   createContext(req, res) {
     const ctx = Object.create(context)
     ctx.request = Object.create(request)
     ctx.response = Object.create(response)
-    ctx.req = req;
-    ctx.res = res;
-    console.log(ctx)
+    ctx.req = ctx.request.req = req;
+    ctx.res = ctx.response.res = res;
+    return ctx;
   }
 
   /**
    * 复合函数
+   * 把每一个异步中间件都展开，如果遇到async，就执行下一个中间件，依此类推，直到执行完所有的中间件，再从
+   * 形成洋葱圈结构
    * @param middlewares 中间件
    */
   compose(middlewares) {
+    return function (ctx) {
+      return dispatch(0)
 
+      function dispatch(i) {
+        const fn = middlewares[i];
+        // 递归终结条件，如果取到了middleware最后一个
+        if (!fn) {
+          return Promise.resolve();
+        }
+
+        return Promise.resolve(
+          fn(ctx, function next() {
+            return dispatch(i + 1)
+          })
+        )
+      }
+    }
   }
 }
 
--- koa-sample/my-koa/test.js
+++ koa-sample/my-koa/test.js
@@ -1,3 +1,33 @@
 const Koa = require('./koa')
 
 const koa = new Koa();
+
+koa.listen(3000, () => {
+  console.log('listen port 3000')
+})
+
+async function fn1(ctx, next) {
+  console.log(1)
+  await next()
+  console.log(6)
+}
+
+async function fn2(ctx, next) {
+  console.log(2)
+  await next()
+  console.log(5)
+}
+
+async function fn3(ctx, next) {
+  console.log(3)
+  await next()
+  console.log(4)
+}
+
+koa.use(fn1).use(fn2).use(fn3)
+
+function delay(ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
+}
