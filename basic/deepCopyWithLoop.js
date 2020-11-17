2379fd8eae9464dfb2baf19e0e24c6437bb6ba2d
weiyb <weiyb@jiedaibao.com>
2020-11-17 10:17:17 +0800

    basic

--- basic/deepCopyWithLoop.js
+++ basic/deepCopyWithLoop.js
@@ -1,53 +1,51 @@
 function getType(data) {
-    return Object.prototype.toString.call(data)
+  return Object.prototype.toString.call(data)
 }
 
 function isObject(data) {
-    return typeof data === 'object' && data !== null
+  return typeof data === 'object' && data !== null
 }
 
 function deepCopy(data) {
-    const root = {};
+  const root = {};
 
-    // 栈
-    const loopList = [
-        {
-            parent: root,
-            key: undefined,
-            data,
-        }
-    ];
+  const stack = [
+    {
+      parent: root,
+      key: undefined,
+      value: data,
+    }
+  ];
 
-    while (loopList.length) {
-        // todo 广度优先和深度优先？
-        const node = loopList.pop();
-        const parent = node.parent;
-        const key = node.key;
-        const data = node.data;
+  while (stack.length) {
+    // todo 广度优先和深度优先？
+    const node = stack.pop();
+    const {parent, key, value} = node;
 
-        // 初始化赋值目标，key为undefined则拷贝到父元素，否则拷贝到子元素
-        let res = parent;
-        if (key !== undefined) {
-            res = parent[key] = {};
-        }
-
-        for (let k in data) {
-            if (data.hasOwnProperty(k)) {
-                if (isObject(data[k])) {
-                    // 下一次循环
-                    loopList.push({
-                        parent: res,
-                        key: k,
-                        data: data[k],
-                    });
-                } else {
-                    res[k] = data[k];
-                }
-            }
-        }
+    // 初始化赋值目标，key为undefined则拷贝到父元素，否则拷贝到子元素
+    let res = parent;
+    if (key !== undefined) {
+      res = parent[key] = {};
     }
 
-    return root;
+    for (let k in value) {
+      if (value.hasOwnProperty(k)) {
+        // 对象就入栈
+        if (isObject(value[k])) {
+          // 下一次循环
+          stack.push({
+            parent: res,
+            key: k,
+            value: data[k],
+          });
+        } else {
+          res[k] = data[k];
+        }
+      }
+    }
+  }
+
+  return root;
 }
 
 
@@ -55,20 +53,20 @@ function deepCopy(data) {
 let obj = {name: 233, gender: 1}
 
 const test = {
-    name: "bruce",
-    // a1: undefined,
-    a2: null,
-    // a3: 123,
-    // a4: '',
-    // a5: () => {
-    // },
-    // a6: obj,
-    // a7: obj,
-    // birth: new Date(1992, 0, 3),
-    // a8: /123$./g,
-    // [Symbol('a9')]: 2,
-    book: {title: "You Don't Know JS", price: "45"},
-    // arr: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]
+  name: "bruce",
+  // a1: undefined,
+  a2: null,
+  // a3: 123,
+  // a4: '',
+  // a5: () => {
+  // },
+  // a6: obj,
+  // a7: obj,
+  // birth: new Date(1992, 0, 3),
+  // a8: /123$./g,
+  // [Symbol('a9')]: 2,
+  book: {title: "You Don't Know JS", price: "45"},
+  // arr: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]
 }
 // test.a4 = test;
 const copy = deepCopy(test)
