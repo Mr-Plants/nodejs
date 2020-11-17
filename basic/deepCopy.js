bc032b7a51c7f1a39177b330e33a356de2a11084
Mr-Plants <wyb19931126/>
2020-11-18 00:26:49 +0800

    basic

--- basic/deepCopy.js
+++ basic/deepCopy.js
@@ -1,71 +1,79 @@
 function getType(data) {
-    return Object.prototype.toString.call(data)
+  return Object.prototype.toString.call(data)
 }
 
 function isObject(data) {
-    return typeof data === 'object' && data !== null
+  // return (typeof data === 'object' || typeof data === 'function') && data !== null
+  return typeof data === 'object' && data !== null
 }
 
 function deepCopy(data, hash = new WeakMap()) {
-    // 简单数据类型，直接返回结果
-    if (!isObject(data)) {
-        return data;
-    }
-    // 如果hash中包含就直接返回，避免重复引用无限递归
-    if (hash.has(data)) {
-        return hash.get(data)
-    }
+  // 简单数据类型，直接返回结果
+  if (!isObject(data)) {
+    return data;
+  }
+  // 如果hash中包含就直接返回，避免重复引用无限递归
+  if (hash.has(data)) {
+    return hash.get(data)
+  }
 
-    // 引用数据类型，先区分是否是特殊对象
-    if (getType(data) === '[object RegExp]') return new RegExp(data)
-    if (getType(data) === '[object Date]') return new Date(data)
-    if (getType(data) === '[object Function]') return new Function(data)
+  // 引用数据类型，先区分是否是特殊对象
+  if (getType(data) === '[object RegExp]') return new RegExp(data)
+  if (getType(data) === '[object Date]') return new Date(data)
+  if (getType(data) === '[object Function]') return new Function(data)
 
-    // todo set、map
-    // 剩下数组和对象
-    let newData = Array.isArray(data) ? [] : {};
+  // todo set、map
+  // 剩下数组和对象
+  let newData = Array.isArray(data) ? [] : {};
 
-    // 保存对象引用
-    hash.set(data, newData);
+  // 保存对象引用
+  hash.set(data, newData);
 
-    // 遍历数据，执行递归
-    // todo 递归爆栈
-    // for (let key in data) {
-    //     // 过滤继承的属性
-    //     if (Object.hasOwnProperty.call(data, key)) {
-    //         newData[key] = deepCopy(data[key], hash)
-    //     }
-    // }
+  // 遍历数据，执行递归
+  // todo 递归爆栈
+  // for (let key in data) {
+  //     // 过滤继承的属性
+  //     if (Object.hasOwnProperty.call(data, key)) {
+  //         newData[key] = deepCopy(data[key], hash)
+  //     }
+  // }
 
-    Reflect.ownKeys(data).filter(item => Object.hasOwnProperty.call(data, item)).forEach(key => {
-        newData[key] = deepCopy(data[key], hash)
-    })
+  Reflect.ownKeys(data).forEach(key => {
+    newData[key] = deepCopy(data[key], hash)
+  })
 
-    return newData
+  return newData
 }
 
 let obj = {name: 233, gender: 1}
 
 const test = {
-    name: "bruce",
-    a1: undefined,
-    a2: null,
-    a3: 123,
-    a4: '',
-    a5: () => {
-    },
-    a6: obj,
-    a7: obj,
-    birth: new Date(1992, 0, 3),
-    a8: /123$./g,
-    [Symbol('a9')]: 2,
-    book: {title: "You Don't Know JS", price: "45"},
-    arr: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]
+  name: "bruce",
+  a1: undefined,
+  a2: null,
+  a3: 123,
+  a4: '',
+  a5: () => {
+  },
+  a6: obj,
+  a7: obj,
+  birth: new Date(1992, 0, 3),
+  a8: /123$./g,
+  [Symbol('a9')]: 2,
+  book: {title: "You Don't Know JS", price: "45"},
+  arr: [{name: 'foo'}, {name: 'bar'}, {name: 'baz'}]
 }
 test.a4 = test;
 const copy = deepCopy(test)
 // console.log(test)
 console.log(copy)
+
+// todo 需要处理function吗
+console.log(copy.a5 === test.a5)
+test.a5 = 666;
+console.log(copy.a5)
+
+
 console.log(test.a6 === test.a7)
 
 console.log(copy.a6 === copy.a7)
--- basic/deepCopy2.js
+++ basic/deepCopy2.js
@@ -1,4 +1,4 @@
-// 使用迭代
+// 使用迭代，单层浅拷贝
 function deepCopy(data) {
   const stack = [data];
   const target = {};
@@ -24,7 +24,8 @@ function deepCopy(data) {
 const testCase = {
   name: 'bruce',
   remark: {
-    favorite: 'basketball'
+    favorite: 'basketball',
+    [Symbol('gender')]: 1
   }
 }
 
@@ -65,3 +66,48 @@ function deepCopy2(data) {
 }
 
 console.log(deepCopy2(testCase))
+
+function getType(data) {
+  return Object.prototype.toString.call(data)
+}
+
+// 兼容数组、symbol、date、正则等
+function deepCopy3(data) {
+  if (typeof data !== 'object' || data === null) {
+    return data;
+  }
+  // todo map set等等构造函数如何处理？
+  if (getType(data) === '[object RegExp]') return new RegExp(data);
+  if (getType(data) === '[object Date]') return new Date(data);
+
+  // 走到这里只剩数组和对象
+  const target = Array.isArray(data) ? [] : {};
+  const stack = [{
+    data,
+    parent: target
+  }];
+
+  while (stack.length) {
+    // 取出栈顶元素
+    const node = stack.pop();
+    const {data, parent} = node;
+
+    Reflect.ownKeys(data).forEach(key => {
+      const value = data[key];
+      if (getType(value) === '[object Object]') {
+        // 先为要保存的键值申请一块内存，并把这个内存地址绑定为下一次要迭代的parent
+        parent[key] = {};
+        // 如果键值是对象，就把它入栈，否则直接拷贝
+        stack.push({
+          data: value,
+          parent: parent[key]
+        })
+      } else {
+        parent[key] = value;
+      }
+    })
+  }
+  return target;
+}
+
+console.log(deepCopy3(testCase))
