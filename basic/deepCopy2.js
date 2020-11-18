62bf4c1c6e6480c5bfa944cc1a9025e2cb6e85fc
Mr-Plants <wyb19931126/>
2020-11-18 13:07:51 +0800

    basic

--- basic/deepCopy2.js
+++ basic/deepCopy2.js
@@ -61,6 +61,8 @@ function deepCopy2(data) {
 // console.log(deepCopy2(testCase))
 const testCase = {
   name: 'bruce',
+  birth: new Date(1993, 11, 26),
+  reg: /123$/g,
   remark: {
     favorite: 'basketball',
     [Symbol('gender')]: 1,
@@ -72,14 +74,27 @@ function getType(data) {
   return Object.prototype.toString.call(data)
 }
 
+function handleObject(data) {
+  if(typeof data!=='object'|| data === null){
+    return data
+  }else {
+    return new data.constructor(data)
+  }
+}
+
 // 兼容数组、symbol、date、正则等
 function deepCopy3(data) {
+  // todo 这里函数也直接返回了
   if (typeof data !== 'object' || data === null) {
     return data;
   }
   // todo map set等等构造函数如何处理？
   if (getType(data) === '[object RegExp]') return new RegExp(data);
   if (getType(data) === '[object Date]') return new Date(data);
+  if (getType(data) === '[object Set]') return new Set(data);
+  if (getType(data) === '[object WeakSet]') return new WeakSet(data);
+  if (getType(data) === '[object Map]') return new Map(data);
+  if (getType(data) === '[object WeakMap]') return new WeakMap(data);
 
   // 走到这里只剩数组和对象
   const target = Array.isArray(data) ? [] : {};
@@ -89,25 +104,29 @@ function deepCopy3(data) {
   }];
 
   while (stack.length) {
-    // console.log(stack)
     // 取出栈顶元素
     const node = stack.pop();
     const {data, parent} = node;
 
     Reflect.ownKeys(data).forEach(key => {
       const value = data[key];
-      // console.log(key, value)
-      if (getType(value) === '[object Object]' || getType(value) === '[object Array]') {
+      const type = getType(value)
+      if (type === '[object Object]' || type === '[object Array]') {
         // 先为要保存的键值申请一块内存，并把这个内存地址绑定为下一次要迭代的parent
         parent[key] = Array.isArray(value) ? [] : {};
-        // console.log(parent[key], key)
         // 如果键值是对象，就把它入栈，否则直接拷贝
         stack.push({
           data: value,
           parent: parent[key]
         })
       } else {
-        parent[key] = value;
+        if (type === '[object RegExp]') {
+          parent[key] = new RegExp(value);
+        } else if (type === '[object Date]') {
+          parent[key] = new Date(value);
+        } else {
+          parent[key] = value;
+        }
       }
     })
   }
@@ -115,4 +134,4 @@ function deepCopy3(data) {
 }
 
 let test = deepCopy3(testCase);
-console.log(JSON.stringify(test))
+console.log(test)
