989a9f3f83782c5e5f9df369090587c783c592b8
weiyb <weiyb@jiedaibao.com>
2020-12-27 18:04:00 +0800

    callback

--- basic/cb.js
+++ basic/cb.js
@@ -1,20 +1,27 @@
-// 同步回调
 function foo(cb) {
   console.log('start')
   cb();
   console.log('end')
 }
 
+// 同步回调，回调函数在主函数体内执行完毕
 function _sync() {
   console.log(2333)
 }
 
-foo(_async)
-
+// 异步回调，回调函数在主函数体外执行完毕
 function _async() {
   setTimeout(function () {
     console.log(2333)
-  }, 0)
+  }, 100)
 }
 
+// start
+// 2333
+// end
 foo(_sync)
+
+// start
+// end
+// 2333
+foo(_async)
