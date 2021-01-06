aac0598854e4ff3d91f50fb5986f97781c65b8a9
Mr-Plants <wyb19931126/>
2021-01-06 09:31:10 +0800

    mathjs

--- mathjs/test.js
+++ mathjs/test.js
@@ -1,5 +1,8 @@
-import {evaluate} from 'mathjs'
+import {format, evaluate, add} from 'mathjs'
 
 // console.log(math)
 // const math = create()
 console.log(evaluate('12 / (2.3 + 0.7)'))
+console.log(format(evaluate('0.1+0.11'),14))
+console.log(format(evaluate('0.1+0.2'),2))
+console.log(add(0.1, 0.11))
